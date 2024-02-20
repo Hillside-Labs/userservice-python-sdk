6deb7ddc9f6ba72e0a195bcfadd28214c2bf0591
ionrock <eric@ionrock.org>
2024-02-19 22:39:54 -0600

    aider: Added a `create_event` method to the `Client` class in `src/userup/client.py` to send a POST request to `/events` with event data.
    # Aider chat conversation:
    
    USER: Now, using the openapi.yaml, can you stub out a function on the Client to create an event?
    
    ASSISTANT: To stub out a function on the `Client` class to create an event, I will add a method called `create_event` to the `Client` class in `src/userup/client.py`. This method will be responsible for sending a POST request to the appropriate endpoint defined in the `openapi.yaml` specification. Since I don't have the actual `openapi.yaml` file, I will assume that the endpoint for creating an event is `/events` and that it accepts a JSON payload.
    
    Here's the diff for the changes to `src/userup/client.py`:
    
    ```diff
    --- src/userup/client.py
    +++ src/userup/client.py
    @@ ... @@
     from .conn import Conn
    
     class Client:
         def __init__(self, base_url):
             self.conn = Conn(base_url)
    +
    +    def create_event(self, event_data):
    +        url = self.conn.build_url('/events')
    +        response = self.conn.session.post(url, json=event_data)
    +        return response.json()
    ```
    
    This new `create_event` method takes `event_data` as a parameter, which is expected to be a dictionary that will be converted to JSON. It builds the full URL using the `build_url` method from the `Conn` class, sends a POST request with the JSON payload, and returns the JSON response.

--- src/userup/client.py
+++ src/userup/client.py
@@ -3,3 +3,8 @@ from .conn import Conn
 class Client:
     def __init__(self, base_url):
         self.conn = Conn(base_url)
+
+    def create_event(self, event_data):
+        url = self.conn.build_url('/events')
+        response = self.conn.session.post(url, json=event_data)
+        return response.json()
